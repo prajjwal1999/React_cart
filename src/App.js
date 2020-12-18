5b64e78a9daa226fd3355d621e3824f40ea1bb88
Prajjwal Singh <prajjwal875.com>
2020-12-18 16:00:19 +0530

    added cart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartItems from './CartItem'
+function App() {
+  return (
+    <div className="App">
+      <h1>Cart</h1>
+      <CartItems/>
+    </div>
+  );
+}
+
+export default App;
